b6c8961050e9cff14ae3be17f109a6e1f74f378c
marcojr73 <marcojuniorengineer@gmail.com>
2023-02-11 10:31:11 -0300

    feat: delete truckes

--- src/services/trucksService.ts
+++ src/services/trucksService.ts
@@ -7,18 +7,34 @@ async function truckAlreadyExist(licensePlate: string) {
     if(truck) errors.conflict("Truck already register on database")
 }
 
+async function isTruckRegister(licensePlate: string) {
+    const truck = await trucksRepository.findTruckByLicensePlate(licensePlate)
+    if(!truck) errors.notFound("Truck not found")
+}
+
 async function registerNewTruck(truck: truckEntity) {
-    await trucksRepository.createTruck(truck)
+    await trucksRepository.upsertTruck(truck)
 }
 
 async function getAllTrucks(page?: number, limit?: number){
     if(!page) page = 1
-    if(!limit) limit = 15
+    if(!limit) limit = 10
     return await trucksRepository.findAllTrucks(page, limit)
+}
+
+async function updateTruck(truck: truckEntity){
+    await trucksRepository.upsertTruck(truck)
+}
+
+async function deleteTruck(licensePlate: string){
+    await trucksRepository.deleteTruckByLicensePlate(licensePlate)
 }
 
 export default {
     truckAlreadyExist,
     registerNewTruck,
-    getAllTrucks
+    getAllTrucks,
+    isTruckRegister,
+    updateTruck,
+    deleteTruck
 }
